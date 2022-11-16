6053ee0d0876e16b2c18892971976773a9271a42
everlaster <everlaster@protonmail.com>
2022-11-17 01:10:00 +0200

    info text

--- src/UI/MainWindow.cs
+++ src/UI/MainWindow.cs
@@ -1,4 +1,5 @@
-﻿using static JointCorrectEE;
+﻿using UnityEngine;
+using static JointCorrectEE;
 
 public class MainWindow : WindowBase
 {
@@ -24,7 +25,8 @@ public class MainWindow : WindowBase
             elements[storable.name] = toggle;
         }
 
-        AddSpacer("lockedFiller", 33, true);
+        AddSpacer("lockedFiller", 57, true);
+        AddSpacer("lockedFiller", 10);
 
         for(int i = 0; i < boneConfigs.Count; i++)
         {
@@ -35,6 +37,16 @@ public class MainWindow : WindowBase
             slider.valueFormat = "F3";
             slider.label = storable.name;
             elements[storable.name] = slider;
+        }
+
+        /* Info */
+        {
+            var storable = new JSONStorableString("infoText", "");
+            storable.val = "\n".Size(12) + "Morphs from package FallenDancer.JointCorrect.11.var.";
+            var textField = jointCorrectEE.CreateTextField(storable);
+            textField.UItext.fontSize = 28;
+            textField.backgroundColor = Color.clear;
+            textField.height = 100;
         }
     }
 }
