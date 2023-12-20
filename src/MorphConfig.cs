41136c62d9d0452fcd43ac8f71944ad5997e06ff
everlaster <everlaster@protonmail.com>
2023-12-20 21:14:26 +0200

    reactivate unloaded demand activated morphs

--- src/MorphConfig.cs
+++ src/MorphConfig.cs
@@ -1,21 +1,49 @@
-﻿sealed class MorphConfig
+﻿using System.Diagnostics.CodeAnalysis;
+
+sealed class MorphConfig
 {
-    readonly DAZMorph _dazMorph;
+    DAZMorph _dazMorph;
+    readonly string _uid;
+    readonly DAZMorphBank _morphBank;
     public JSONStorableFloat groupMultiplierJsf;
+    bool _enabled = true;
 
     public MorphConfig(DAZMorph dazMorph)
     {
         _dazMorph = dazMorph;
+        _uid = dazMorph.uid;
+        _morphBank = dazMorph.morphBank;
     }
 
     public void Update(float value, float min, float max)
     {
         value = Calc.NormalizeFloat(value, min, max);
-        _dazMorph.morphValue = groupMultiplierJsf.val * value;
+        SetValue(groupMultiplierJsf.val * value);
     }
 
-    public void Reset()
+    void SetValue(float value)
     {
-        _dazMorph.morphValue = 0;
+        if(!_enabled)
+        {
+            return;
+        }
+
+        if(_dazMorph.isDemandLoaded && !_dazMorph.isDemandActivated)
+        {
+            _dazMorph = _morphBank.GetMorphByUid(_uid); // Reactivates the morph in case it has been unloaded by e.g. memory optimization
+            if(_dazMorph == null)
+            {
+                new LogBuilder().ErrorNoReport("Morph with uid '{0}' not found!", _uid);
+                _enabled = false;
+                return;
+            }
+        }
+
+        _dazMorph.morphValue = value;
     }
+
+    [SuppressMessage("ReSharper", "UnusedMember.Global")]
+    public float GetValue() => _dazMorph.morphValue;
+
+    public void Reset() => _dazMorph.morphValue = 0;
 }
