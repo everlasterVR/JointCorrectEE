d21fe1f30c66e91068c935ada71f96428cd27ffa
everlaster <everlaster@protonmail.com>
2023-12-21 10:43:50 +0200

    update packaging script

--- scripts/package.sh
+++ scripts/package.sh
@@ -2,6 +2,7 @@
 
 set -e
 
+./scripts/generate_cslist.sh
 author_name=everlaster
 resource_name=JointCorrectEE
 
@@ -15,17 +16,17 @@ plugin_version=$(git describe --tags --match "v*" --abbrev=0 HEAD 2>/dev/null | 
 work_dir=publish
 mkdir -p work_dir
 
-resource_dir=$work_dir/Custom/Scripts/$author_name/$resource_name
-mkdir -p $resource_dir
-cp meta.json $work_dir/
-cp ./*.cslist $resource_dir/
-cp -r src $resource_dir/
+resource_dir="$work_dir"/Custom/Scripts/"$author_name"/"$resource_name"
+mkdir -p "$resource_dir"
+cp meta.json "$work_dir"/
+cp ./*.cslist "$resource_dir"/
+cp -r src "$resource_dir"/
 
 # update version info
-sed -i "s/0\.0\.0/$plugin_version/g" $work_dir/meta.json
-sed -i "s/0\.0\.0/$plugin_version/g" $resource_dir/src/$resource_name.cs
+sed -i "s/0\.0\.0/$plugin_version/g" "$work_dir"/meta.json
+sed -i "s/0\.0\.0/$plugin_version/g" "$resource_dir"/src/ScriptBase.cs
 
-for file in $(find $resource_dir -type f -name "*.cs"); do
+for file in $(find "$resource_dir" -type f -name "*.cs"); do
     # set production env
     sed -i "s/#define ENV_DEVELOPMENT/\/\//" "$file"
     # hide .cs files (plugin is loaded with .cslist)
@@ -34,16 +35,16 @@ done
 
 # zip files to .var and cleanup
 printf "Creating package...\n"
-package_file="$author_name.$resource_name.$package_version.var"
-cd $work_dir
+package_file="$author_name"."$resource_name"."$package_version".var
+cd "$work_dir"
 zip -rq "$package_file" ./*
 printf "Package %s created for plugin version v%s.\n" "$package_file" "$plugin_version"
 mv "$package_file" ..
 cd ..
-rm -rf $work_dir
+rm -rf "$work_dir"
 
 # move archive to AddonPackages
 addon_packages_dir=../../../../AddonPackages/Self
-mkdir -p $addon_packages_dir
+mkdir -p "$addon_packages_dir"
 mv "$package_file" $addon_packages_dir
 printf "Package %s moved to AddonPackages/Self.\n" "$package_file"
